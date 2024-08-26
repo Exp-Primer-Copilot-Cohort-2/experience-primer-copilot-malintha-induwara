81413e0e6ad5903ebd27875d71fd73a59f08d11e
Malintha Induwara <induwara2k@gmail.com>
2024-08-26 09:22:53 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,36 @@
+function skillsMember() {
+  return {
+    skills: [
+      {
+        name: 'JavaScript',
+        level: 'intermediate',
+        experience: '4 years'
+      },
+      {
+        name: 'CSS',
+        level: 'intermediate',
+        experience: '4 years'
+      },
+      {
+        name: 'HTML',
+        level: 'intermediate',
+        experience: '4 years'
+      },
+      {
+        name: 'React',
+        level: 'intermediate',
+        experience: '4 years'
+      },
+      {
+        name: 'Node',
+        level: 'intermediate',
+        experience: '4 years'
+      },
+      {
+        name: 'Express',
+        level: 'intermediate',
+        experience: '4 years'
+      }
+    ]
+  }
+}
